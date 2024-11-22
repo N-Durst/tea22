c264d0b1b328c3c425b0fc8a684c240feac31ab1
N-Durst <147518612+N-Durst@users.noreply.github.com>
2024-11-22 09:45:59 +0100

    ka

--- exercise-006/linkedList.cpp
+++ exercise-006/linkedList.cpp
@@ -3,13 +3,10 @@
 #include <stdio.h>      /* printf, scanf, NULL */
 #include <stdlib.h>     /* malloc, free, rand */
 
-typedef struct List {
-    struct ListNode_t* pHead;
-    struct ListNode_t* pTail;
-    unsigned int size;
-} List_t;
 
-typedef struct ListNode {
-    unsigned int data;
-    struct ListNode* pNext;
-} ListNode_t;
+
+
+
+
+
+int insertIntoLinkedList(LinkedList* List, unsigned int data){}
--- exercise-006/linkedList.h
+++ exercise-006/linkedList.h
@@ -1,6 +1,21 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
+
+typedef struct ListNode {
+    unsigned int data;
+    struct ListNode* pNext;
+}ListNode_t;
+
+
+typedef struct List {
+    ListNode_t* pHead;
+    ListNode_t* pTail;
+    unsigned int size;
+}List_t;
+
+
+
 int insertIntoLinkedList(linkedList* List, unsigned int data);
 
 int removeFromLinkedList(linkedList* List, ListNode* elem);
